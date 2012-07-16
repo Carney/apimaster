0b1468d34d25435d798912faa48cbec890916ff5
Xiqiu Sun <sunxiqiu@admaster.com.cn>
2012-07-16 17:45:56 +0800

    Remove user_mock

--- lib/apimaster.rb
+++ lib/apimaster.rb
@@ -6,17 +6,19 @@ module Apimaster end
 module Apimaster::Helpers end
 module Apimaster::Controllers end
 module Apimaster::Models end
+module Apimaster::Mocks end
+module Apimaster::Test end
 
 require_relative './apimaster/setting'
 require_relative './apimaster/error'
 require_relative './apimaster/mapper'
 
-require_relative './apimaster/helpers/headers.rb'
-require_relative './apimaster/helpers/request.rb'
-require_relative './apimaster/helpers/session.rb'
+require_relative './apimaster/helpers/headers'
+require_relative './apimaster/helpers/request'
+require_relative './apimaster/helpers/session'
 
-require_relative './apimaster/models/user.rb'
-require_relative './apimaster/models/user_mock.rb'
+require_relative './apimaster/models/user'
 
-require_relative './apimaster/controllers/errors.rb'
+require_relative './apimaster/controllers/errors'
 require_relative './apimaster/application'
+
